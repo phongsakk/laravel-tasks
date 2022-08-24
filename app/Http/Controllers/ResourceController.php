eee482a4224aa8878b0c5f12ce1dfe7a35000981
Phongsakk <phongsak.ysd@gmail.com>
2022-08-24 14:53:02 +0700

    add course resource

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Course;
 use App\Models\Prefix;
+use App\Models\User;
 
 class ResourceController extends Controller
 {
@@ -14,6 +15,23 @@ class ResourceController extends Controller
 
     public function courses()
     {
-        return Course::with(['contents', 'user'])->get();
+        $searchText = request()->get('search');
+        $searchQuery = '%' . $searchText . '%';
+        $users = User::where('id', 'like', $searchQuery)
+            ->orWhere('firstname', 'like', $searchQuery)
+            ->orWhere('lastname', 'like', $searchQuery)
+            ->get();
+        $courses = Course::with(['author'])
+        ->where('id', 'like', $searchQuery)
+        ->orWhere('name', 'like', $searchQuery)
+        ->orWhere('description', 'like', $searchQuery);
+        if ($users->count() > 0) foreach ($users as $user) {
+            $courses->orWhereBelongsTo($user,'author');
+        }
+        $courses = $courses->paginate(2);
+        return response([
+            'query' => $searchText,
+            'paginate' => $courses,
+        ]);
     }
 }
